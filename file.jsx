f34af2167ff65e50a09be474c9afe804e144625a
varannchik <varannchik@gmail.com>
2020-01-21 21:13:36 +0200

    alll

---
+++ file.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+
+export default class extends Component{
+    constructor(props){
+        super(props);
+        // this.state = {
+        //     value:{
+        //         a:''
+        //     }           
+        // }
+        this.state = {
+            value:''   
+        }
+        this.handleChange = this.handleChange.bind(this);
+    };
+
+    handleChange(event) {
+        // this.setState({value:{a:''}: event.target.value.a});
+         this.setState({value: event.target.value});
+    }
+
+    // shouldComponentUpdate(nextProps,nextState){
+    //     // if(Number(nextState.value) >10){
+    //     //     return false
+    //     // }else{
+    //     //     return true
+    //     // }
+    //     if(nextProps.value===this.state.value){
+    //         return false
+    //     }
+    // }
+
+    render(){
+        // console.log(this.state.value.a);
+        console.log(this.state.value);
+        return (
+            <div>
+                {/* <input type="text" value={this.state.value.a} onChange={this.handleChange}/>
+                <div>{this.state.value.a}</div> */}
+
+                <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                <div>{this.state.value}</div>
+            </div>        
+        )
+    }
+    
+}
